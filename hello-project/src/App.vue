ef3b8e53af96bc73f1fdaa0d909e6e63dcae073f
vrana <vibhanshurana10@gmail.com>
2021-03-06 16:05:03 +0530

    002_PROJECT

---
+++ hello-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+ <h1>Hellow Vue Js</h1> 
+ <Hello :homeTitle=" homeTitle" :allUsers="users"/>
+</template>
+
+<script>
+import Hello from "./components/Hello"
+
+export default {
+  name: 'App',
+  components: {
+   Hello
+  },
+  data(){
+    return{
+       homeTitle: "Hello my first vue cli app",
+      users: ["david rana" ,"shuhana devi","jeenu rana"],
+      };
+      },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-project/src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="hello">
+        <h1>{{homeTitle}}</h1>
+        <p>Hello vue js student</p>
+        <h4 v-for="(user,index) in allUsers" :key="index"> {{index}} = {{user}} </h4>
+        <button @click="callFunction">Click me</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: ["homeTitle","allUsers"],
+  data(){
+      return{
+          title: "My first vue cli application",
+          };
+    },
+     methods:{
+        callFunction: function(){
+          console.log("hello click enent");
+        },
+      },
+};
+</script>
+
+
+<style>
+h1{
+    color: green;
+}
+</style>
